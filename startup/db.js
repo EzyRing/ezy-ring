9c26b730855a6d3b03c183db0769b92df8f673e2
Haris Ali <khansahab.ahad@gmail.com>
2020-08-09 17:31:03 +0500

    adding connection string and enviorment var

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,12 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
 const Fawn = require("fawn");
+const config = require("config");
 
 module.exports = function () {
   mongoose
-    .connect("mongodb://localhost/ezy-ring")
-    .then(() => winston.info("Connected to MongoDB --> EZY-RING.DB"));
+    .connect(config.get("db"))
+    .then(() => winston.info("Connected to MongoDB --> " + config.get("db")));
 
   Fawn.init(mongoose);
 };
